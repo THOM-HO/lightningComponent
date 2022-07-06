345a678d9a34b341d6c0bd5d40f2c173d432c3ab
ThOM HO <thomho1602@gmail.com>
2022-07-06 14:04:54 +0700

    createnew

--- force-app/main/default/aura/CreateStudentAuraComponent/CreateStudentAuraComponentController.js
+++ force-app/main/default/aura/CreateStudentAuraComponent/CreateStudentAuraComponentController.js
@@ -1,11 +1,9 @@
 ({
-    onRecordSubmit: function(component, event, helper) {
-        event.preventDefault();
-        const fields = event.getParam('fields');
-        // fields.ContactId = component.get("v.recordId");
-        component.find('recordEditForm').submit(fields);
-    },
-    onRecordSuccess : function(component,event,helper) {
-       console.log('done')
-   }
+    handleSuccess : function(component, event, helper) {
+        component.find('notifLib').showToast({
+            "variant": "success",
+            "title": "Student Created",
+            "message": "Record ID: " + event.getParam("id")
+        });
+    }
 })
