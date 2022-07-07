ff4b2cd4020012cb2cd8e520e362f81bf89c0d44
ThOM HO <thomho1602@gmail.com>
2022-07-07 08:38:06 +0700

    khac

--- force-app/main/default/aura/SearchAuraComponent/SearchAuraComponentHelper.js
+++ force-app/main/default/aura/SearchAuraComponent/SearchAuraComponentHelper.js
@@ -1,5 +1,15 @@
 ({
-    helperMethod : function() {
-
-    }
+    getAccounts : function(component) {
+		var action = component.get('c.searchAccount');
+        action.setParams({ searchCondition : component.get("v.searchCondition") });
+        action.setCallback(this, function(actionResult){
+            var state = actionResult.getState();
+            console.log('===state get accounts===' + state);
+            if (state === "SUCCESS")
+            {
+                component.set('v.accounts', actionResult.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+	}
 })
